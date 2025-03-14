0c67053714259cb67c9d17b45aa77c128d179c44
Adrian Canals <a.canals14@gmail.com>
2025-03-14 09:56:25 +0100

    nullableEnums: Modificar la clase "EnumDynamicVo" para establecerla como "$nullable = false" y crear la nueva clase "EnumDynamicNullVo" para cuando pueda ser nullable

--- src/Domain/Objects/ValueObjects/Primitives/EnumDynamicNullVo.php
+++ src/Domain/Objects/ValueObjects/Primitives/EnumDynamicNullVo.php
@@ -6,12 +6,6 @@ namespace Thehouseofel\Kalion\Domain\Objects\ValueObjects\Primitives;
 
 use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Primitives\Contracts\ContractDynamicEnumVo;
 
-class EnumDynamicVo extends ContractDynamicEnumVo
+class EnumDynamicNullVo extends ContractDynamicEnumVo
 {
-    protected bool $nullable = false;
-
-    public function value(): string
-    {
-        return $this->value;
-    }
 }
