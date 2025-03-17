a939abda3160f08281d7385f6f9ca2c6e5d2634d
Adrian Canals <a.canals14@gmail.com>
2025-03-17 22:28:10 +0100

    (refactor) Auth: Mover blade de "pages.login.fake.index" a "pages.login.fake"

--- src/Infrastructure/Http/Controllers/Web/AuthController.php
+++ src/Infrastructure/Http/Controllers/Web/AuthController.php
@@ -26,7 +26,7 @@ final class AuthController extends Controller
     public function create(): View
     {
         if (config('kalion_auth.login.fake')) {
-            return view('kal::pages.login.fake.index');
+            return view('kal::pages.login.fake');
         }
 
         throw new FeatureUnavailableException();
