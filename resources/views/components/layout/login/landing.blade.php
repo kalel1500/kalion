5ff15e5ec456e5fab8d96d76c04f282cb00a0c21
Adrian Canals <a.canals14@gmail.com>
2025-03-17 23:09:37 +0100

    Auth: Modificar propiedad "lang" del html de la layout "layout.login.landing" para que la obtenga dinámicamente

--- resources/views/components/layout/login/landing.blade.php
+++ resources/views/components/layout/login/landing.blade.php
@@ -1,7 +1,7 @@
 @use('Thehouseofel\Kalion\Infrastructure\Services\Renderer')
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
