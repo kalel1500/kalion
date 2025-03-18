fa71302877d368742894d159b79846ce90094744
Adrian Canals <a.canals14@gmail.com>
2025-03-18 12:19:15 +0100

    Rehacer por completo el comando "LogsClear" * Comprobar si existe la carpeta "Logs" * Vaciar todos los logs de la carpeta en vez de solo el "laravel.log" * Usar el Filesystem ("File::put()") en vez de ejecutar el "echo '' > logs/laravel.log" * Ajustar permisos solo en Linux, ya que en Windows no hace falta y asi evitar el fallo que daba en windows * Usar el "shell_exec" en vez del "exec" para ejecutar el comando "chmod" * Asignar permisos "775" en vez de "777" * Dejar de guardar Logs con el resultado tanto "try" como en el "catch"

--- src/Infrastructure/Console/Commands/LogsClear.php
+++ src/Infrastructure/Console/Commands/LogsClear.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace Thehouseofel\Kalion\Infrastructure\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
-use Throwable;
+use Illuminate\Support\Facades\File;
 
 final class LogsClear extends Command
 {
@@ -29,17 +28,32 @@ final class LogsClear extends Command
      */
     public function handle()
     {
+        $logPath = storage_path('logs');
+
+        // Limpiar archivos de log
+        if (!File::exists($logPath)) {
+            $this->warn('La carpeta de logs no existe.');
+            return;
+        }
+
+        foreach (File::files($logPath) as $file) {
+            File::put($file->getPathname(), ''); // Vaciar el contenido del archivo
+        }
+
+        $this->info('Logs have been cleared');
+
+        // Ajustar permisos (solo en Linux/Mac)
+        if (so_is_windows()) {
+            // $this->info('En Windows, no es necesario ajustar permisos.');
+            return;
+        }
+
         try {
-//            exec('rm ' . storage_path('logs/*.log'));
-            exec('echo "" > ' . storage_path('logs/laravel.log'));
-            exec('chmod -R 777 ' . storage_path());
-            Log::info('Logs limpios');
-            $this->comment('Logs have been cleared!');
-        } catch (Throwable $exception) {
-//            $message = 'El comando "rm" no existe';
-            $message = $exception->getMessage();
-            Log::warning($message);
-            $this->comment($message);
+            shell_exec("chmod -R 775 {$logPath}");
+            // shell_exec("chown -R www-data:www-data {$logPath}"); // Sirve para asegurar que el usuario y el grupo de Apache tengan la propiedad del archivo (hay que ajustar según el servidor)
+            $this->info('Permisos ajustados correctamente.');
+        } catch (\Exception $e) {
+            $this->error('No se pudieron ajustar los permisos: ' . $e->getMessage());
         }
     }
 }
