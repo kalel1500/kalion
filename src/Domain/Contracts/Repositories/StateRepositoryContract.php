8db7f345a661fb9a9783dfea2036a763d0dbbbf9
Adrian Canals <a.canals14@gmail.com>
2025-03-27 10:37:10 +0100

    (breaking) Eliminar funcionalidad del "EnumDynamic", ya que no termina de funcionar

--- src/Domain/Contracts/Repositories/StateRepositoryContract.php
+++ src/Domain/Contracts/Repositories/StateRepositoryContract.php
@@ -6,15 +6,10 @@ namespace Thehouseofel\Kalion\Domain\Contracts\Repositories;
 
 use Thehouseofel\Kalion\Domain\Objects\Entities\Collections\StateCollection;
 use Thehouseofel\Kalion\Domain\Objects\Entities\StateEntity;
-use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Parameters\StatePluckFieldVo;
-use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Parameters\StatePluckKeyVo;
-use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Primitives\EnumDynamicVo;
 
 interface StateRepositoryContract
 {
     public function all(): StateCollection;
-    public function getDictionary(StatePluckFieldVo $field, StatePluckKeyVo $key): array;
-    public function getDictionaryByType(EnumDynamicVo $type, StatePluckFieldVo $field, StatePluckKeyVo $key): array;
-    public function getByType(EnumDynamicVo $type): StateCollection;
-    public function findByCode(EnumDynamicVo $code): StateEntity;
+    public function searchByType(string $type): StateCollection;
+    public function findByCode(string $code): StateEntity;
 }
--- src/Infrastructure/Repositories/StateEloquentRepository.php
+++ src/Infrastructure/Repositories/StateEloquentRepository.php
@@ -4,14 +4,9 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Infrastructure\Repositories;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Thehouseofel\Kalion\Domain\Contracts\Repositories\StateRepositoryContract;
-use Thehouseofel\Kalion\Domain\Exceptions\Database\RecordNotFoundException;
 use Thehouseofel\Kalion\Domain\Objects\Entities\Collections\StateCollection;
 use Thehouseofel\Kalion\Domain\Objects\Entities\StateEntity;
-use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Parameters\StatePluckFieldVo;
-use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Parameters\StatePluckKeyVo;
-use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Primitives\EnumDynamicVo;
 use Thehouseofel\Kalion\Infrastructure\Models\State;
 
 class StateEloquentRepository implements StateRepositoryContract
@@ -29,29 +24,19 @@ class StateEloquentRepository implements StateRepositoryContract
         return StateCollection::fromEloquent($eloquentResult);
     }
 
-    public function getDictionary(StatePluckFieldVo $field, StatePluckKeyVo $key): array
-    {
-        return $this->all()->pluck($field->value(), $key->value())->toArray();
-    }
-
-    public function getDictionaryByType(EnumDynamicVo $type, StatePluckFieldVo $field, StatePluckKeyVo $key): array
-    {
-        return $this->getByType($type)->pluck($field->value(), $key->value())->toArray();
-    }
-
-    public function getByType(EnumDynamicVo $type): StateCollection
+    public function searchByType(string $type): StateCollection
     {
         $eloquentResult = $this->eloquentModel::query()
-            ->where('type', $type->value())
+            ->where('type', $type)
             ->get();
 
         return StateCollection::fromEloquent($eloquentResult);
     }
 
-    public function findByCode(EnumDynamicVo $code): StateEntity
+    public function findByCode(string $code): StateEntity
     {
         $eloquentResult = $this->eloquentModel::query()
-            ->where('code', $code->value())
+            ->where('code', $code)
             ->firstOrFail();
         return StateEntity::fromArray($eloquentResult->toArray());
     }
