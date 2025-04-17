6ed63ce993a916751cb7a2fda71021cf62c97dd3
Adrian Canals <a.canals14@gmail.com>
2025-04-17 14:55:17 +0200

    layout: Mejora componente "dark-mode.blade.php" para que busque el "theme" en el "localStorage" si no lo encuentra en el "html"

--- resources/views/components/js/dark-mode.blade.php
+++ resources/views/components/js/dark-mode.blade.php
@@ -1,6 +1,6 @@
 <!-- JavaScript para cargar el DarkMode -->
 <script>
-    const theme = document.querySelector('html').getAttribute('color-theme');
+    const theme = document.querySelector('html').getAttribute('color-theme') ?? localStorage.theme;
     if (theme === 'dark' || (theme === 'system' && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
         document.documentElement.classList.add("dark");
         document.documentElement.setAttribute("data-theme", "dark");
