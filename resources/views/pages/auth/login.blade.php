d7eab11ce05b86fc506cec0211ef088cfc87977c
Adrian Canals <a.canals14@gmail.com>
2025-04-17 23:54:07 +0200

    Auth: Añadir funcionalidad básica al formulario "pages/auth/login"

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,9 +1,26 @@
+@use(Thehouseofel\Kalion\Infrastructure\Services\Kalion)
+@php($field = Kalion::getLoginFieldData())
+
 <x-kal::layout.guest title="Login" cardTitle="Sign in to your account">
 
-    <form class="space-y-4 md:space-y-6" action="#">
+    <form method="POST" action="{{ route('login') }}" class="space-y-4 md:space-y-6">
+        @csrf
         <div>
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+            <label for="{{ $field->name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $field->label }}</label>
+            <input
+                    type="{{ $field->type }}"
+                    name="{{ $field->name }}"
+                    id="{{ $field->name }}"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="{{ $field->placeholder }}"
+                    value="{{ old($field->name) }}"
+                    required
+            />
+            @error($field->name)
+                <div class="p-2 mt-1 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-900 dark:text-red-400" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
         </div>
         <div>
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -12,7 +29,7 @@
         <div class="flex items-center justify-between">
             <div class="flex items-start">
                 <div class="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="">
+                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                 </div>
                 <div class="ml-3 text-sm">
                     <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
