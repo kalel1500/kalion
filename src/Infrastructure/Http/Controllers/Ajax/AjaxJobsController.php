00a292ba802f8842d046ba824d14539a0ac14b96
Adrian Canals <a.canals14@gmail.com>
2025-04-17 11:06:20 +0200

    jobs: Inyectar los "UseCases" en el "AjaxJobsController" en vez de instanciarlos y pasarle el repository

--- src/Infrastructure/Http/Controllers/Ajax/AjaxJobsController.php
+++ src/Infrastructure/Http/Controllers/Ajax/AjaxJobsController.php
@@ -7,38 +7,32 @@ namespace Thehouseofel\Kalion\Infrastructure\Http\Controllers\Ajax;
 use Illuminate\Http\JsonResponse;
 use Thehouseofel\Kalion\Application\GetAllFailedJobsUseCase;
 use Thehouseofel\Kalion\Application\GetAllJobsUseCase;
-use Thehouseofel\Kalion\Domain\Contracts\Repositories\JobRepositoryContract;
 use Thehouseofel\Kalion\Infrastructure\Http\Controllers\Controller;
 use Throwable;
 
 final class AjaxJobsController extends Controller
 {
-    public function __construct(private JobRepositoryContract $repositoryJob)
+    public function __construct(
+        private readonly GetAllJobsUseCase $getAllJobsUseCase,
+        private readonly GetAllFailedJobsUseCase $getAllFailedJobsUseCase,
+    )
     {
     }
 
-    /**
-     * @throws Throwable
-     */
     public function getJobs(): JsonResponse
     {
         try {
-            $getAllJobsUseCase = new GetAllJobsUseCase($this->repositoryJob);
-            $jobs              = $getAllJobsUseCase->__invoke();
+            $jobs = $this->getAllJobsUseCase->__invoke();
             return response_json(true, 'success', ['jobs' => $jobs->toArray()]);
         } catch (Throwable $th) {
             return response_json_error($th);
         }
     }
 
-    /**
-     * @throws Throwable
-     */
     public function getFailedJobs(): JsonResponse
     {
         try {
-            $getAllFailedJobsUseCase = new GetAllFailedJobsUseCase($this->repositoryJob);
-            $jobs                    = $getAllFailedJobsUseCase->__invoke();
+            $jobs = $this->getAllFailedJobsUseCase->__invoke();
             return response_json(true, 'success', ['jobs' => $jobs->toArray()]);
         } catch (Throwable $th) {
             return response_json_error($th);
