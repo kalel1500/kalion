190b96590ac7308684eca37202f6cc90f36afc76
Adrian Canals <a.canals14@gmail.com>
2025-04-18 05:15:37 +0200

    components: Añadir clase "font-medium" al componente "link"

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -8,6 +8,6 @@
     $normal_class = $underline ? 'underline' : '';
     $hoover_class = $underline ? 'hover:no-underline' : 'hover:underline';
     $extra_class = $tag !== 'a' ? 'cursor-pointer' : '';
-    $classes = "text-blue-600 $normal_class dark:text-blue-500 $hoover_class $extra_class";
+    $classes = "font-medium text-blue-600 $normal_class dark:text-blue-500 $hoover_class $extra_class";
 @endphp
 <{{ $tag }} @if($tag === 'a') href="{{ $href }}" @endif {{ $attributes->mergeTailwind($classes) }}>{{ $slot }}</{{ $tag }}>
