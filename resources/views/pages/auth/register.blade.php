70a3e63865d544de54d6db720f5a0f434449757d
Adrian Canals <a.canals14@gmail.com>
2025-04-18 21:30:55 +0200

    Auth: vista "register" pasada a componentes

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -3,33 +3,29 @@
     <form method="POST" action="{{ route('register') }}" class="space-y-4 md:space-y-6">
         @csrf
         <div>
-            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-            <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name Surname" required>
+            <x-kal::input.label for="name" value="Your name"/>
+            <x-kal::input.text id="name" placeholder="Name Surname" required/>
         </div>
         <div>
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required>
+            <x-kal::input.label for="email" value="Your email"/>
+            <x-kal::input.email id="email" placeholder="name@company.com" required/>
         </div>
         <div>
-            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <x-kal::input.label for="password" value="Password"/>
+            <x-kal::input.password id="password" placeholder="••••••••" required/>
         </div>
         <div>
-            <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+            <x-kal::input.label for="password_confirmation" value="Confirm password"/>
+            <x-kal::input.password id="password_confirmation" placeholder="••••••••" required/>
         </div>
-        <div class="flex items-start">
-            <div class="flex items-center h-5">
-                <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
-            </div>
-            <div class="ml-3 text-sm">
-                <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the <a class="font-medium text-blue-600 hover:underline dark:text-blue-500" href="#">Terms and Conditions</a></label>
-            </div>
-        </div>
-        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create an account</button>
-        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-            Already have an account? <a href="{{ route('login') }}" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Login here</a>
-        </p>
+
+        <x-kal::input.full.checkbox id="terms" required>
+            <span class="font-light">I accept the</span> <x-kal::link value="Terms and Conditions"/>
+        </x-kal::input.full.checkbox>
+
+        <x-kal::form.button>Create an account</x-kal::form.button>
+
+        <x-kal::form.question-link value="Already have an account?" link="Login here" href="{{ route('login') }}"/>
     </form>
 
 </x-kal::layout.guest>
