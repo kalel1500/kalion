c56953974c01c9f8b862785091188b288b563114
Adrian Canals <a.canals14@gmail.com>
2025-04-20 16:09:55 +0200

    Auth: mensajes de "error" añadidos a la vista "register"

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -5,23 +5,28 @@
         <div>
             <x-kal::input.label for="name" value="Your name"/>
             <x-kal::input.text id="name" placeholder="Name Surname" required/>
+            <x-kal::input.error for="name" />
         </div>
         <div>
             <x-kal::input.label for="email" value="Your email"/>
             <x-kal::input.email id="email" placeholder="name@company.com" required/>
+            <x-kal::input.error for="email" />
         </div>
         <div>
             <x-kal::input.label for="password" value="Password"/>
             <x-kal::input.password id="password" placeholder="••••••••" required/>
+            <x-kal::input.error for="password" />
         </div>
         <div>
             <x-kal::input.label for="password_confirmation" value="Confirm password"/>
             <x-kal::input.password id="password_confirmation" placeholder="••••••••" required/>
+            <x-kal::input.error for="password_confirmation" />
         </div>
 
         <x-kal::input.full.checkbox id="terms" required>
             <span class="font-light">I accept the</span> <x-kal::link value="Terms and Conditions"/>
         </x-kal::input.full.checkbox>
+        <x-kal::input.error for="terms" />
 
         <x-kal::form.button>Create an account</x-kal::form.button>
 
