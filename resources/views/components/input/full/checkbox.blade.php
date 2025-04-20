e1ef002502b61e7045ac2f755726a3f279ff8a3e
Adrian Canals <a.canals14@gmail.com>
2025-04-20 17:53:00 +0200

    (fix-prev) components: Añadir atributo "name" al input del componente "input.full.checkbox"

--- resources/views/components/input/full/checkbox.blade.php
+++ resources/views/components/input/full/checkbox.blade.php
@@ -4,6 +4,7 @@
     <div class="flex items-center h-5">
         <input
             id="{{ $id }}"
+            name="{{ $id }}"
             aria-describedby="{{ $id }}"
             type="checkbox"
             class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
