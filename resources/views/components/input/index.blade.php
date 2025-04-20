5ebafc30a31939ffb1192dc8012f030ca0d49a25
Adrian Canals <a.canals14@gmail.com>
2025-04-20 16:00:54 +0200

    components: color borde input cambiado de "blue-600" a "blue-500"

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -4,7 +4,7 @@
     type="{{ $type }}"
     name="{{ $attributes->get('id') ?? $attributes->get('name') }}"
     {{ $attributes->only('id') }}
-    {{ $attributes->only('class')->mergeTailwind('bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500') }}
+    {{ $attributes->only('class')->mergeTailwind('bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500') }}
     {{ $attributes->except(['id', 'class']) }}
     @disabled($disabled)
     @required($required)
