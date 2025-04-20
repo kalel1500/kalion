b64887df8a4bf3c9db4f90ba0c5cdde81f95a8f8
Adrian Canals <a.canals14@gmail.com>
2025-04-20 16:54:20 +0200

    components: Añadir borde rojo al componente "input" si el campo que recibe contiene algún error

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -1,10 +1,21 @@
 @props(['type', 'required' => false, 'disabled' => false])
 
+@php
+    $name = $attributes->get('id') ?? $attributes->get('name');
+    $messages = $errors->get($name);
+
+    $ringClasses = 'focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500';
+    $errorClasses = 'border-red-500 focus:ring-red-500 focus:border-red-500 dark:border-red-500';
+
+    $other = 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white';
+    $final = $other.' '.($messages ? $errorClasses : $ringClasses);
+@endphp
+
 <input
     type="{{ $type }}"
-    name="{{ $attributes->get('id') ?? $attributes->get('name') }}"
+    name="{{ $name }}"
     {{ $attributes->only('id') }}
-    {{ $attributes->only('class')->mergeTailwind('bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500') }}
+    {{ $attributes->only('class')->mergeTailwind($final) }}
     {{ $attributes->except(['id', 'class']) }}
     @disabled($disabled)
     @required($required)
