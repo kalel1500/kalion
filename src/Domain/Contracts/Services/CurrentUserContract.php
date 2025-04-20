f27f526a52a9e5f4d4441a959c30e433c151a998
Adrian Canals <a.canals14@gmail.com>
2025-04-21 01:29:05 +0200

    Eliminar el "template" del "PhpDoc" para indicar el tipo "UserEntity"

--- src/Domain/Contracts/Services/CurrentUserContract.php
+++ src/Domain/Contracts/Services/CurrentUserContract.php
@@ -4,15 +4,7 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Domain\Contracts\Services;
 
-use Thehouseofel\Kalion\Domain\Objects\Entities\UserEntity;
-
-/**
- * @template T of UserEntity
- */
 interface CurrentUserContract
 {
-    /**
-     * @return T|null
-     */
     public function entity(string $guard = null);
 }
--- src/Infrastructure/Services/Auth/CurrentUser.php
+++ src/Infrastructure/Services/Auth/CurrentUser.php
@@ -5,33 +5,23 @@ declare(strict_types=1);
 namespace Thehouseofel\Kalion\Infrastructure\Services\Auth;
 
 use Thehouseofel\Kalion\Domain\Contracts\Services\CurrentUserContract;
-use Thehouseofel\Kalion\Domain\Objects\Entities\UserEntity;
 use Thehouseofel\Kalion\Infrastructure\Services\Kalion;
 
-/**
- * @template T of UserEntity
- */
 final class CurrentUser implements CurrentUserContract
 {
-    private bool $loadRoles;
+    private bool    $loadRoles;
     private string  $entityClass;
     private ?string $guard;
-
-    /** @var UserEntity|null */
-    private $userEntity = null;
+    private mixed   $userEntity = null;
 
     public function __construct()
     {
         $this->loadRoles = config('kalion.auth.load_roles');
     }
 
-    /**
-     * @param string|null $guard
-     * @return UserEntity|null
-     */
     public function entity(string $guard = null)
     {
-        $this->guard = $guard;
+        $this->guard       = $guard;
         $this->entityClass = Kalion::getClassUserEntity($this->guard);
 
         if ($this->userEntity && $this->userEntity->getGuard() === $this->guard) {
@@ -48,7 +38,7 @@ final class CurrentUser implements CurrentUserContract
             $user->load('roles');
             $with = ['roles'];
         }
-        $this->userEntity =  $this->entityClass::fromArray($user->toArray(), $with);
+        $this->userEntity = $this->entityClass::fromArray($user->toArray(), $with);
         $this->userEntity->setGuard($guard);
         return $this->userEntity;
     }
