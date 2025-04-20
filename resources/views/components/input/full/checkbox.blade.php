00a2ea821cd6ce27888cf14e15ad458b3f4d1559
Adrian Canals <a.canals14@gmail.com>
2025-04-20 18:01:25 +0200

    components: Igualar lógica del componente "input.full.checkbox" a la del "input"

--- resources/views/components/input/full/checkbox.blade.php
+++ resources/views/components/input/full/checkbox.blade.php
@@ -1,19 +1,26 @@
-@props(['id', 'labelText', 'required' => false, 'disabled' => false])
+@props(['labelText', 'required' => false, 'disabled' => false])
+
+@php
+    $name = $attributes->get('id') ?? $attributes->get('name');
+
+    $classes = 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800';
+@endphp
 
 <div class="flex items-start">
     <div class="flex items-center h-5">
         <input
-            id="{{ $id }}"
-            name="{{ $id }}"
-            aria-describedby="{{ $id }}"
             type="checkbox"
-            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+            name="{{ $name }}"
+            {{ $attributes->only('id') }}
+            aria-describedby="{{ $name }}"
+            {{ $attributes->only('class')->mergeTailwind($classes) }}
+            {{ $attributes->except(['id', 'class']) }}
             @disabled($disabled)
             @required($required)
         >
     </div>
     <div class="ml-3 text-sm">
-        <label for="{{ $id }}" class="text-gray-500 dark:text-gray-300">
+        <label for="{{ $name }}" class="text-gray-500 dark:text-gray-300">
             {{ $labelText ?? $slot }}
             {{ $error ?? '' }}
         </label>
