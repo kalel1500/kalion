571054e3ac36bed5debe71a559fafaa1a3f695d7
Adrian Canals <a.canals14@gmail.com>
2025-04-30 22:13:17 +0200

    (refactor) Refactorizar clase "RedirectAfterLogin" extrayendo lógica común a la clase abstracta "Redirector" para mejorar reutilización y mantener consistencia en redirecciones

---
+++ src/Infrastructure/Services/Config/Redirect/Redirector.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thehouseofel\Kalion\Infrastructure\Services\Config\Redirect;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+abstract class Redirector
+{
+    /**
+     * The callback that should be used to generate the redirect path.
+     *
+     * @var callable|null
+     */
+    protected static $redirectToCallback;
+
+    /**
+     * Get the path the user should be redirected to
+     */
+    abstract public static function redirectTo(Request $request = null): ?string;
+
+    /**
+     * Get the default URI the user should be redirected to
+     */
+    protected static function defaultRedirectUri(): string
+    {
+        foreach (['dashboard', 'home'] as $uri) {
+            if (Route::has($uri)) {
+                return route($uri);
+            }
+        }
+
+        $routes = Route::getRoutes()->get('GET');
+
+        foreach (['dashboard', 'home'] as $uri) {
+            if (isset($routes[$uri])) {
+                return '/'.$uri;
+            }
+        }
+
+        return '/';
+    }
+
+    /**
+     * Specify the callback that should be used to generate the redirect path.
+     */
+    public static function redirectUsing(callable $redirectToCallback): void
+    {
+        static::$redirectToCallback = $redirectToCallback;
+    }
+}
