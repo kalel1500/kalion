fee9ea7f1e6dd7a27ad945e28505597c69a37c8e
Adrian Canals <a.canals14@gmail.com>
2025-05-02 20:21:53 +0200

    layout: Mejorar estilos "sidebar" cuando está colapsado (centrar texto cuando hay saltos de línea): * Botón del dropdown: Permitir saltos de línea y centrar texto [eliminar: "whitespace-nowrap", añadir: "sc:text-center"] * Enlace: Centrar texto de primer nivel cuando no tiene counter [añadir: "'text-center' => (!isset($counter) && $level === '0')"]

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -25,7 +25,7 @@
     @if($isDropdown)
         <button type="button" class="{{ $linkClasses }}" aria-controls="dropdown-{{ $dropdownId }}" data-collapse-toggle="dropdown-{{ $dropdownId }}">
             {!! $iconHtml !!}
-            <span @class([$spanClasses, 'flex-1 whitespace-nowrap text-left'])>{{ $slot }}</span>
+            <span @class([$spanClasses, 'flex-1 text-left sc:text-center'])>{{ $slot }}</span>
 {{--            <svg aria-hidden="true" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>--}}
             <x-kal::icon.chevron-down outline @class(['size-4', 'chevron-down', '-rotate-90' => $dropdownIsOpen]) stroke-width="3.5"/>
         </button>
@@ -39,7 +39,7 @@
                 {{ $slot }}
             @else
                 {!! $iconHtml !!}
-                <span @class([$spanClasses, 'flex-1 whitespace-nowrap' => isset($counter)])>{{ $slot }}</span>
+                <span @class([$spanClasses, 'flex-1 whitespace-nowrap' => isset($counter), 'text-center' => (!isset($counter) && $level === '0')])>{{ $slot }}</span>
                 @isset($counter)
                     <span class="text-blue-800 bg-blue-100 dark:bg-blue-200 dark:text-blue-800 inline-flex h-5 w-5 items-center justify-center rounded-full text-xs font-semibold md:sc:hidden">{{ $counter }}</span>
                 @endisset
