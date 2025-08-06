5fbefd0cee3ad50deef564a6569b06d15942988f
Adrian Canals <a.canals14@gmail.com>
2025-08-06 16:07:25 +0200

    (refactor) Replace qualifier with an import

--- src/Domain/Traits/CountMethods.php
+++ src/Domain/Traits/CountMethods.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Domain\Traits;
 
+use ReflectionClass;
+
 trait CountMethods
 {
     protected function countPublicMethods(): int
@@ -12,7 +14,7 @@ trait CountMethods
 //        dd($numeroDeMetodos);
 
         // Crear una instancia de ReflectionClass para la clase actual
-        $reflexion = new \ReflectionClass($this);
+        $reflexion = new ReflectionClass($this);
 
         // Obtener los métodos públicos o protegidos (sin incluir los estáticos)
         $methods = $reflexion->getMethods(\ReflectionMethod::IS_PUBLIC);
