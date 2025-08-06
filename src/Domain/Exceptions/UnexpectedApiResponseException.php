7e122aa32982184316a7d773d79de366e01d40ca
Adrian Canals <a.canals14@gmail.com>
2025-08-06 15:12:25 +0200

    (breaking) Hacer que la Excepción "UnexpectedApiResponseException" para que extienda de la nueva "KalionException" en vez de la "KalionRuntimeException"

--- src/Domain/Exceptions/UnexpectedApiResponseException.php
+++ src/Domain/Exceptions/UnexpectedApiResponseException.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Domain\Exceptions;
 
-use Thehouseofel\Kalion\Domain\Exceptions\Base\KalionRuntimeException;
+use Thehouseofel\Kalion\Domain\Exceptions\Base\KalionException;
 
-final class UnexpectedApiResponseException extends KalionRuntimeException
+final class UnexpectedApiResponseException extends KalionException
 {
     const STATUS_CODE = 502; // Response::HTTP_BAD_GATEWAY
 }
