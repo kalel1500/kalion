141effbfe209e17539689567b20349a77ab9c065
Adrian Canals <a.canals14@gmail.com>
2025-08-06 15:10:05 +0200

    (breaking) Hacer que la Interfaz "KalionExceptionInterface" extienda de "Throwable"

--- src/Domain/Contracts/KalionExceptionInterface.php
+++ src/Domain/Contracts/KalionExceptionInterface.php
@@ -4,7 +4,7 @@ namespace Thehouseofel\Kalion\Domain\Contracts;
 
 use Thehouseofel\Kalion\Domain\Objects\DataObjects\ExceptionContextDo;
 
-interface KalionExceptionInterface
+interface KalionExceptionInterface extends \Throwable
 {
     public function getStatusCode(): int;
     public function getContext(): ?ExceptionContextDo;
