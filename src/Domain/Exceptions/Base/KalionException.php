65cb966b38a3c29528f65378dffdcb93b6b5d3fd
Adrian Canals <a.canals14@gmail.com>
2025-08-06 15:10:37 +0200

    Nueva excepción "KalionException" que extiende de Exception (para no tener solo las que extienden de "RuntimeException" y "LogicException")

---
+++ src/Domain/Exceptions/Base/KalionException.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thehouseofel\Kalion\Domain\Exceptions\Base;
+
+use Exception;
+use Thehouseofel\Kalion\Domain\Contracts\KalionExceptionInterface;
+use Thehouseofel\Kalion\Domain\Traits\KalionExceptionBehavior;
+use Throwable;
+
+abstract class KalionException extends Exception implements KalionExceptionInterface
+{
+    use KalionExceptionBehavior;
+
+    public function __construct(
+        ?string    $message = null,
+        ?Throwable $previous = null,
+        int        $code = 0,
+        ?array     $data = null,
+        bool       $success = false
+    )
+    {
+        $this->initKalionException(
+            static::STATUS_CODE,
+            $message ?? static::MESSAGE,
+            $previous,
+            $code,
+            $data,
+            $success
+        );
+    }
+}
