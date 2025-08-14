2b94a3ef776de7bf01ada73ccd54231f2444fe91
Adrian Canals <a.canals14@gmail.com>
2025-08-14 09:00:30 +0200

    Se ha eliminado el modificador "final" de la clase "ResponseBasicDo" para que se pueda extender

--- src/Domain/Objects/DataObjects/Responses/ResponseBasicDo.php
+++ src/Domain/Objects/DataObjects/Responses/ResponseBasicDo.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Domain\Objects\DataObjects\Responses;
 
-final class ResponseBasicDo extends ContractResponseDefaultDo
+class ResponseBasicDo extends ContractResponseDefaultDo
 {
 }
