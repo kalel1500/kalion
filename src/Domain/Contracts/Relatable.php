75e4031a7d0808fd54ec559e6aa962c8fb4a5b49
Adrian Canals <a.canals14@gmail.com>
2025-09-05 14:27:50 +0200

    Definir los métodos "setWith()", "setIsFull()" y "fromArray()" en la interfaz "Relatable"

--- src/Domain/Contracts/Relatable.php
+++ src/Domain/Contracts/Relatable.php
@@ -6,4 +6,15 @@ namespace Thehouseofel\Kalion\Domain\Contracts;
 
 interface Relatable
 {
+    public function setWith(string|array|null $with): static;
+    public function setIsFull(bool|string|null $isFull): static;
+
+    /**
+     * @template T of array|null
+     * @param T $data
+     * @param string|array|null $with
+     * @param bool|string $isFull
+     * @return (T is null ? null : static)
+     */
+    public static function fromArray($data, string|array|null $with = null, bool|string $isFull = null);
 }
