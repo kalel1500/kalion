715d09ccdf83f2867bec66bbf0d2075906588880
Adrian Canals <a.canals14@gmail.com>
2025-09-17 20:12:11 +0200

    Añadir el método "fromArray()" a la interfaz "ArrayConvertible"

--- src/Domain/Contracts/ArrayConvertible.php
+++ src/Domain/Contracts/ArrayConvertible.php
@@ -6,5 +6,11 @@ namespace Thehouseofel\Kalion\Domain\Contracts;
 
 interface ArrayConvertible
 {
+    /**
+     * @template T of array|null
+     * @param T $data
+     * @return (T is null ? null : static)
+     */
+    public static function fromArray(?array $data): ?static;
     public function toArray(): array;
 }
