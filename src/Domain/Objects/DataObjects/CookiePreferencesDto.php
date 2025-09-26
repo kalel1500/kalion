fd08d18c8ca983404408df298948ff2abc1937e2
Adrian Canals <a.canals14@gmail.com>
2025-09-26 11:32:12 +0200

    (warn) Marcar la clase CookiePreferencesDto como "@internal" con DocBlock

--- src/Domain/Objects/DataObjects/CookiePreferencesDto.php
+++ src/Domain/Objects/DataObjects/CookiePreferencesDto.php
@@ -6,6 +6,9 @@ namespace Thehouseofel\Kalion\Domain\Objects\DataObjects;
 
 use Thehouseofel\Kalion\Domain\Objects\ValueObjects\Parameters\ThemeVo;
 
+/**
+ * @internal This class is not meant to be used or overwritten outside the package.
+ */
 final class CookiePreferencesDto extends AbstractDataTransferObject
 {
     public function __construct(
