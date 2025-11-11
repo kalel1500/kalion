84304aa2b72911c8f43a97db18b43e4970321a89
Adrian Canals <a.canals14@gmail.com>
2025-11-11 21:44:57 +0100

    moverClasesFeature: Se han movido los controllers de Auth a la carpeta "Feature"

--- src/Features/Auth/Infrastructure/Http/Controllers/Web/Auth/LoginController.php
+++ src/Features/Auth/Infrastructure/Http/Controllers/Web/Auth/LoginController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Thehouseofel\Kalion\Core\Infrastructure\Http\Controllers\Web\Auth;
+namespace Thehouseofel\Kalion\Features\Auth\Infrastructure\Http\Controllers\Web\Auth;
 
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
