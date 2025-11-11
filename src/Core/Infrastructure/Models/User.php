88b1fe96448cd8d59133024410ca38ce809b227a
Adrian Canals <a.canals14@gmail.com>
2025-11-11 20:17:38 +0100

    (fix-prev) Corregir import del "UserFactory" en el modelo "User" (se había colado el Core durante el refactor anterior)

--- src/Core/Infrastructure/Models/User.php
+++ src/Core/Infrastructure/Models/User.php
@@ -7,7 +7,7 @@ namespace Thehouseofel\Kalion\Core\Infrastructure\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Thehouseofel\Kalion\Core\Database\Factories\UserFactory;
+use Thehouseofel\Kalion\Database\Factories\UserFactory;
 use Thehouseofel\Kalion\Core\Infrastructure\Models\Concerns\ModelHasPermissions;
 
 class User extends Authenticatable
