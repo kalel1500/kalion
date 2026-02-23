a4aecf8d04b8a50e3f3d9bcf2c0097749369a68a
Adrian Canals <a.canals14@gmail.com>
2026-02-23 21:52:40 +0100

    install: actualizar archivos

--- stubs/generate/base/routes/web.php
+++ stubs/generate/base/routes/web.php
@@ -2,17 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/**
- * Ruta original de Laravel para la vista welcome
- */
-
 Route::get('/welcome', fn() => view('welcome'))->name('welcome');
 
-/**
- * Rutas de la aplicación
- */
-
-// Rutas protegidas
+// Protected routes
 Route::middleware(['auth'])->group(function () {
     //
 });
