029e2d119f8f60d12ed8fc7c57c86ea2adf65dca
Adrian Canals <a.canals14@gmail.com>
2026-03-03 22:53:53 +0100

    (breaking) layout: Renombrar config "kalion.layout.blade_show_main_border" a "kalion.layout.show_debug_main_border".

--- config/kalion.php
+++ config/kalion.php
@@ -168,7 +168,7 @@ return [
         'use_elevated_shadows' => (bool) env('KALION_LAYOUT_USE_ELEVATED_SHADOWS', false), // por si crece: 'shadow_style' => 'default', // default | elevated | none
         'sidebar_initial_state' => env('KALION_LAYOUT_SIDEBAR_INITIAL_STATE', SidebarState::expanded->value),  // expanded | collapsed
         'sidebar_state_per_page' => (bool) env('KALION_LAYOUT_SIDEBAR_STATE_PER_PAGE', false),
-        'blade_show_main_border' => (bool) env('KALION_LAYOUT_BLADE_SHOW_MAIN_BORDER', false),
+        'show_debug_main_border' => (bool) env('KALION_LAYOUT_SHOW_DEBUG_MAIN_BORDER', false),
         'service' => env('KALION_LAYOUT_SERVICE', $defaults['kalion.layout.service']),
         'asset_path_logo' => env('KALION_LAYOUT_ASSET_PATH_LOGO', 'resources/images/logo.svg'),
         'asset_path_favicon' => env('KALION_LAYOUT_ASSET_PATH_FAVICON', 'resources/images/favicon.ico'),
