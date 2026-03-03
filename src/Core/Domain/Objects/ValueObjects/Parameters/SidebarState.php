b8877e1c5795fe9eee8d67017e09543eab698663
Adrian Canals <a.canals14@gmail.com>
2026-03-03 23:45:19 +0100

    Añadir trait "HasFromOr" a los enums "SidebarState" y "ThemeVo"

--- src/Core/Domain/Objects/ValueObjects/Parameters/SidebarState.php
+++ src/Core/Domain/Objects/ValueObjects/Parameters/SidebarState.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Core\Domain\Objects\ValueObjects\Parameters;
 
+use Thehouseofel\Kalion\Core\Domain\Concerns\Enums\HasFromOr;
+
 /**
  * @internal This class is not meant to be used or overwritten outside the package.
  */
 enum SidebarState: string
 {
+    use HasFromOr;
+
     case expanded  = 'expanded';
     case collapsed = 'collapsed';
 
