d8f73879b72c56e55dd553c51654450c6d471aab
Adrian Canals <a.canals14@gmail.com>
2026-03-04 18:07:04 +0100

    layout: quitar margen del nombre de la App del navbar, ya que no siempre estará el buscador y puede que no se necesite

--- resources/views/components/navbar/brand.blade.php
+++ resources/views/components/navbar/brand.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ safe_route('index', '/') }}" class="mr-4 hidden xs:flex items-center justify-between lg:mr-20">
+<a href="{{ safe_route('index', '/') }}" class="mr-4 hidden xs:flex items-center justify-between">
     <img src="@viteAsset(config('kalion.layout.logo_path'))" width="32" height="33" class="mr-3 h-8" alt="{{ config('app.name') }} Logo"/>
     <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">{{ config('app.name') }}</span>
 </a>
