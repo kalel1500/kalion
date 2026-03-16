ad9cdfe4b0a98c3ee2dd5005d6710fe519d96c62
Adrian Canals <a.canals14@gmail.com>
2026-03-16 17:24:15 +0100

    (breaking) components: Actualizar estilos "input.full.checkbox"

--- resources/views/components/input/full/checkbox.blade.php
+++ resources/views/components/input/full/checkbox.blade.php
@@ -1,28 +1,22 @@
-@props(['labelText', 'required' => false, 'disabled' => false])
+@php /** @var \Illuminate\View\ComponentAttributeBag $attributes */ @endphp
+@php /** @var \Illuminate\Support\ViewErrorBag $errors */ @endphp
 
-@php
-    $name = $attributes->get('id') ?? $attributes->get('name');
+@props(['labelText', 'id' => null, 'name' => null, 'required' => false, 'disabled' => false])
 
-    $classes = 'w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800';
-@endphp
+@php($identifier = $id ?? $name ?? '')
 
-<div class="flex items-start">
-    <div class="flex items-center h-5">
-        <input
-            type="checkbox"
-            name="{{ $name }}"
-            {{ $attributes->only('id') }}
-            aria-describedby="{{ $name }}"
-            {{ $attributes->only('class')->mergeTailwind($classes) }}
-            {{ $attributes->except(['id', 'class']) }}
-            @disabled($disabled)
-            @required($required)
-        >
-    </div>
-    <div class="ml-3 text-sm">
-        <label for="{{ $name }}" class="text-gray-500 dark:text-gray-300">
-            {{ $labelText ?? $slot }}
-            {{ $error ?? '' }}
-        </label>
-    </div>
+<div class="flex items-center">
+    <input
+        type="checkbox"
+        id="{{ $identifier }}"
+        name="{{ $identifier }}"
+        {{ $attributes->mergeTailwind('w-4 h-4 border border-default-medium rounded-xs bg-neutral-secondary-medium focus:ring-2 focus:ring-brand-soft') }}
+        @disabled($disabled)
+        @required($required)
+    >
+
+    <label for="{{ $name }}" class="ms-2 text-sm font-medium text-heading select-none">
+        {{ $labelText ?? $slot }}
+        {{ $error ?? '' }}
+    </label>
 </div>
