e63a88315098b78952afc6415825786e6ff58413
Adrian Canals <a.canals14@gmail.com>
2026-03-16 17:19:48 +0100

    (breaking) components: Actualizar estilos "card" + Se ha rehecho el componente "card" quitándole el título y el párrafo.

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,3 @@
-@props(['title', 'contentId' => ''])
-
-<div {{ $attributes->mergeTailwind('block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-900 dark:border-gray-700 dark:shadow-2xl') }}>
-    @isset($title)
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $title }}</h5>
-    @endisset
-    <p class="font-normal text-gray-700 dark:text-gray-400" id="{{ $contentId }}">{{ $slot }}</p>
+<div {{ $attributes->mergeTailwind('bg-neutral-primary-medium block max-w-sm p-6 border border-default-medium rounded-base shadow-xs dark:shadow-glow-1') }}>
+    {{ $slot }}
 </div>
