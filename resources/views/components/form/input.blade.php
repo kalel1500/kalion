c825b31463809248c36af540acf10b7d8e490015
Adrian Canals <a.canals14@gmail.com>
2026-03-18 19:07:52 +0100

    components: añadir parámetro "size" al componente "form.input"

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,11 +1,17 @@
 @php /** @var \Illuminate\View\ComponentAttributeBag $attributes */ @endphp
 @php /** @var \Illuminate\Support\ViewErrorBag $errors */ @endphp
 
-@props(['type', 'id' => null, 'name' => null, 'value' => '', 'required' => false, 'disabled' => false])
+@props(['type', 'id' => null, 'name' => null, 'value' => '', 'size' => 'base', 'required' => false, 'disabled' => false])
 
 @php
     $identifier = $id ?? $name ?? '';
-    $common     = 'border text-sm rounded-base block w-full px-3 py-2.5 shadow-xs';
+    $sizeClasses = [
+        'small' => 'text-sm   px-2.5 py-2',
+        'base'  => 'text-sm   px-3   py-2.5',
+        'large' => 'text-base px-3.5 py-3',
+        'extra' => 'text-base px-4   py-3.5',
+    ];
+    $common     = 'border rounded-base block w-full shadow-xs' . ' ' . $sizeClasses[$size];
     $normal     = 'bg-neutral-secondary-medium border-default-medium text-heading focus:ring-brand focus:border-brand placeholder:text-body';
     $error      = 'bg-danger-soft border-danger-subtle text-fg-danger-strong focus:ring-danger focus:border-danger placeholder:text-fg-danger-strong';
     $classes    = $common.' '.($errors->has($identifier) ? $error : $normal);
