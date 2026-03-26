63efd427b67988926331588609c405cefbdc1c2e
Adrian Canals <a.canals14@gmail.com>
2026-03-26 11:57:21 +0100

    (refactor) components: Adaptar componentes usando las clases de Tailwind 4

--- resources/views/components/layout/guest.blade.php
+++ resources/views/components/layout/guest.blade.php
@@ -18,21 +18,22 @@
         <!-- CSS del paquete -->
         {!! PackageAssets::css() !!}
     </head>
-    <body class="bg-gray-50 dark:bg-gray-900">
+    <body class="bg-neutral-secondary-soft">
 
         <section >
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-heading">
                     <img class="w-8 h-8 mr-2" src="@viteAsset(config('kalion.layout.logo_path'))" alt="logo">
                     {{ config('app.name', 'Laravel 12') }}
                 </a>
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+
+                <div class="w-full sm:max-w-md md:mt-0 xl:p-0 bg-neutral-primary-medium rounded-base shadow border border-default-medium">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <div>
-                            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            <h1 class="text-xl font-bold leading-tight tracking-tight text-heading md:text-2xl">
                                 {{ $cardTitle }}
                             </h1>
-                            <p class="mt-1 font-light text-gray-500 dark:text-gray-400">{{ $cardText }}</p>
+                            <p class="mt-1 font-light text-body-subtle">{{ $cardText }}</p>
                         </div>
 
                         {{ $slot }}
