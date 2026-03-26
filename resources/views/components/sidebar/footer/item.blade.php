c98ebee1188b3e0888615fda6fc269bd2987e0df
Adrian Canals <a.canals14@gmail.com>
2026-03-26 09:50:03 +0100

    (refactor) components: Adaptar componentes usando las clases de Tailwind 4

--- resources/views/components/sidebar/footer/item.blade.php
+++ resources/views/components/sidebar/footer/item.blade.php
@@ -3,7 +3,7 @@
 @php
     $isDropdown = isset($dropdown);
     $hasTooltip = isset($tooltip);
-    $itemClasses = 'inline-flex cursor-pointer justify-center rounded-sm p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white';
+    $itemClasses = 'inline-flex cursor-pointer justify-center rounded-sm p-2 text-body-subtle hover:bg-neutral-tertiary-medium hover:text-heading';
 @endphp
 
 @if(!$isDropdown)
