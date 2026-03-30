71a6f608c707ae80ab3dfef69387251d65caf0f7
Adrian Canals <a.canals14@gmail.com>
2026-03-30 15:36:08 +0200

    (refactor) Añadir comentarios en el componente "navbar.theme-toggle"

--- resources/views/components/navbar/theme-toggle.blade.php
+++ resources/views/components/navbar/theme-toggle.blade.php
@@ -8,14 +8,17 @@
     <x-fwb-s-sun id="theme-toggle-light-icon" @class(['hidden' => !$dark_theme]) />
 </x-kal::navbar.item>--}}
 
+<!-- Nav item (dark) -->
 <x-kal::navbar.item id="theme-dark" tooltip="{{ __('Switch to light mode') }}" @class(['hidden', 'block!' => $theme->isDark()])>
     <x-fwb-s-moon />
 </x-kal::navbar.item>
 
+<!-- Nav item (light) -->
 <x-kal::navbar.item id="theme-light" tooltip="{{ __('Switch to system mode') }}" @class(['hidden', 'block!' => $theme->isLight()])>
     <x-fwb-s-sun />
 </x-kal::navbar.item>
 
+<!-- Nav item (system) -->
 <x-kal::navbar.item id="theme-system" tooltip="{{ __('Switch to dark mode') }}" @class(['hidden', 'block!' => $theme->isSystem()])>
     <x-bi-circle-half />
 </x-kal::navbar.item>
