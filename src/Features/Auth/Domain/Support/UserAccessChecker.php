2f12b784a33a15a042ce8a5e031f0a523e1e1618
Adrian Canals <a.canals14@gmail.com>
2026-04-03 00:52:59 +0200

    (refactor) Simplificar "UserAccessChecker" unificando los métodos "userHasPermission" + "userHasRole" en el nuevo "userHas" y moviendo la lógica de "is" y "can" al nuevo "check"

--- src/Features/Auth/Domain/Support/UserAccessChecker.php
+++ src/Features/Auth/Domain/Support/UserAccessChecker.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Features\Auth\Domain\Support;
 
-use Thehouseofel\Kalion\Features\Auth\Domain\Objects\Entities\PermissionEntity;
-use Thehouseofel\Kalion\Features\Auth\Domain\Objects\Entities\RoleEntity;
+use Thehouseofel\Kalion\Core\Domain\Exceptions\NeverCalledException;
+use Thehouseofel\Kalion\Features\Auth\Domain\Contracts\AccessEntity;
 use Thehouseofel\Kalion\Features\Auth\Domain\Objects\Entities\UserEntity;
 
 /**
@@ -14,43 +14,39 @@ use Thehouseofel\Kalion\Features\Auth\Domain\Objects\Entities\UserEntity;
 final readonly class UserAccessChecker
 {
     public function __construct(
-        private PermissionParser     $permissionParser
+        private PermissionParser $permissionParser
     )
     {
     }
 
     public function can(UserEntity $user, string|array $permissions, array $params): bool
     {
-        $array_permissions = $this->permissionParser->getArrayPermissions($permissions, $params);
-        return $array_permissions->contains(fn($params, $permission) => $this->userHasPermission($user, $permission, $params));
+        return $this->check('can', $user, $permissions, $params);
     }
 
     public function is(UserEntity $user, string|array $roles, array $params): bool
     {
-        $array_roles = $this->permissionParser->getArrayPermissions($roles, $params);
-        return $array_roles->contains(fn($params, $role) => $this->userHasRole($user, $role, $params));
+        return $this->check('is', $user, $roles, $params);
     }
 
-    protected function userHasPermission(UserEntity $user, string $permission, array $params = []): bool
+    protected function check(string $method, UserEntity $user, string|array $value, array $params): bool
     {
-        return $user->permissions()->contains(function (PermissionEntity $userPermission) use ($user, $permission, $params) {
+        $values = $this->permissionParser->getArrayPermissions($value, $params);
+        return $values->contains(fn($params, $value) => $this->userHas($method, $user, $value, $params));
+    }
+
+    protected function userHas(string $item, UserEntity $user, string $value, array $params = []): bool
+    {
+        if (! in_array($item, ['permissions', 'roles'])) {
+            throw new NeverCalledException(sprintf('The method %s is not meant to be called with the item "%s".', __METHOD__, $item));
+        }
+
+        return $user->$item()->contains(function (AccessEntity $item) use ($user, $value, $params) {
             $repositoryUser = new (kauth($user->getGuard())->getClassUserRepository());
 
-            if ($userPermission->name->value !== $permission) return false;
-            if ($userPermission->getIsQuery()) return $repositoryUser->{$permission}($user, ...$params);
+            if ($item->name->value !== $value) return false;
+            if ($item->getIsQuery()) return $repositoryUser->{$value}($user, ...$params);
             return true;
         });
     }
-
-    protected function userHasRole(UserEntity $user, string $role, array $params = []): bool
-    {
-        return $user->roles()->contains(function (RoleEntity $userRole) use ($user, $role, $params) {
-            $repositoryUser = new (kauth($user->getGuard())->getClassUserRepository());
-
-            if ($userRole->name->value !== $role) return false;
-            if ($userRole->getIsQuery()) return $repositoryUser->{$role}($user, ...$params);
-            return true;
-        });
-    }
-
 }
