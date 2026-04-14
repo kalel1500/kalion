3838d668011b44985b7d3d13bc1dc52e3a9de0ed
Adrian Canals <a.canals14@gmail.com>
2026-04-14 12:07:16 +0200

    (breaking) components: eliminar "from.button" ya que no hace falta

--- resources/views/components/form/button.blade.php
+++
@@ -1,5 +0,0 @@
-@php /** @var \Illuminate\View\ComponentSlot $slot */ @endphp
-@php /** @var \Illuminate\View\ComponentAttributeBag $attributes */ @endphp
-@php /** @var \Illuminate\Support\ViewErrorBag $errors */ @endphp
-
-<x-kal::button type="submit" {{ $attributes->twMerge('w-full text-center') }}>{{ $slot }}</x-kal::button>
