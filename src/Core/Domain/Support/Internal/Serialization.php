e40066eb25191c8b6d9357b03f0393f1baf70152
Adrian Canals <a.canals14@gmail.com>
2026-07-28 20:07:09 +0200

    (fix) support: mejorar métodos de la clase "Serialization" para que solo devuelvan array u objeto si la conversión ha ido bien. Si no devuelven null

--- src/Core/Domain/Support/Internal/Serialization.php
+++ src/Core/Domain/Support/Internal/Serialization.php
@@ -4,26 +4,40 @@ declare(strict_types=1);
 
 namespace Thehouseofel\Kalion\Core\Domain\Support\Internal;
 
+use JsonException;
+
 /**
  * @internal This class is intended for internal package usage only.
  */
 final class Serialization
 {
-    public static function jsonToArray(mixed $object): array|object|null
+    public static function jsonToArray(mixed $object): ?array
     {
-        $string = json_encode($object);
-        if (! $string) {
+        try {
+            $decoded = json_decode(
+                json       : json_encode($object, JSON_THROW_ON_ERROR),
+                associative: true,
+                flags      : JSON_THROW_ON_ERROR,
+            );
+
+            return is_array($decoded) ? $decoded : null;
+        } catch (JsonException) {
             return null;
         }
-        return json_decode($string, true);
     }
 
-    public static function jsonToObject(mixed $object): array|object|null
+    public static function jsonToObject(mixed $object): ?object
     {
-        $string = json_encode($object);
-        if (! $string) {
+        try {
+            $decoded = json_decode(
+                json       : json_encode($object, JSON_THROW_ON_ERROR),
+                associative: false,
+                flags      : JSON_THROW_ON_ERROR,
+            );
+
+            return is_object($decoded) ? $decoded : null;
+        } catch (JsonException) {
             return null;
         }
-        return json_decode($string);
     }
 }
