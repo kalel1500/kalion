8d9954f16483a503573b0b4adf14d5554d38b57a
Adrian Canals <a.canals14@gmail.com>
2026-07-30 09:21:57 +0200

    (fix) steps: Corregir imports js

--- install/stubs/generate/base/vite.config.ts
+++ install/stubs/generate/base/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import { kalionJs } from '@kalel1500/kalion-js/dist/plugins/vite';
+import { kalionJs } from '@kalel1500/kalion-js/vite-plugin';
 
 export default defineConfig({
     plugins: [
